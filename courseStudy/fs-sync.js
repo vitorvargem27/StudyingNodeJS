dced7f1cc7816b6a53c73142d78045f39eddc0cb
vitorVargem <tp.vitor.vargem@gmail.com>
2024-02-26 17:25:05 -0300

    add Arquivo com Blocking

---
+++ courseStudy/fs-sync.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+console.log((process.hrtime()[0]/[60]).toFixed(5));
+console.log("Antes da execução...")
+
+const data = fs.readFileSync("C:\\Users\\vitor.vargem\\Desktop\\estudosNodeJS\\StudyingNodeJS\\courseStudy\\file.txt");
+console.log("Executando o script após o arquivo...");
+
+console.log((process.hrtime()[0]/[60]).toFixed(5));
+
+//Transformando o buffer do arquivo em texto
+const transformBuffer = Buffer.from(data, 'utf8')
+const bufferText = transformBuffer.toString('utf-8')
+console.log(bufferText)
---
+++ courseStudy/fsWithoutSync.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+//Tempo antes de er o arquivo
+console.log("Tempo antes da execução...")
+console.log((process.hrtime()[0]/[60]).toFixed(5));
+
+//Tempo depois de ler o arquivo
+const data = fs.readFile("C:\\Users\\vitor.vargem\\Desktop\\estudosNodeJS\\StudyingNodeJS\\courseStudy\\file.txt");
+console.log("Tempo executando o script após o arquivo ser lido...");
+console.log((process.hrtime()[0]/[60]).toFixed(5));
+
